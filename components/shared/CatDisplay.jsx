beade2da685f9c26b4744b0f34fc8eba0b18a701
LShuyanLi <sli341@my.bcit.ca>
2026-05-10 15:39:08 -0700

    Update cat display

--- components/shared/CatDisplay.jsx
+++ components/shared/CatDisplay.jsx
@@ -1,6 +1,23 @@
 // CatDisplay.jsx: Displays the selected cat image on the play screen.
 
+import { useEffect } from "react";
+
+
+
 export default function CatDisplay({ selectedCat }) {
+
+    // const {emotion} = useCatContext();
+    // const [catfaceimg, setCatfaceimg] = useState("/sad.png");
+
+    // useEffect(() => {
+    //     // This effect runs whenever the selected cat changes, allowing us to trigger any necessary updates or animations based on the new cat selection.
+    //     // For example, we could add a simple animation or sound effect here when the cat changes.
+    //     console.log("Selected cat changed:", selectedCat);
+    //     if(emotion === "happy"){   
+    //         setCatfaceimg("/happy.png");
+    //     }
+
+    // }, [emotion]);
   return (
     <div
       style={{
@@ -11,6 +28,10 @@ export default function CatDisplay({ selectedCat }) {
         zIndex: 10,
       }}
     >
+        {/* <img src={catfaceimg} style={{
+            position: "absolute",
+            zIndex: 15,
+        }} /> */}
       <img
         src={selectedCat}
         style={{
